c5f6f24dabd3c458ec5e39a7f740c28a437ab705
sriramsv <sriramsv1991@gmail.com>
2017-05-30 00:10:49 -0700

    Fix

--- my_assist/reminder/views.py
+++ my_assist/reminder/views.py
@@ -2,7 +2,7 @@
 """ Reminder section """
 from flask import Blueprint,jsonify,request
 import requests,json
-from models import statereminder
+from .models import statereminder
 from my_assist.extensions import manager
 from models import db
 
