f75b3cb0dac3c9b456c4ace32936b05915f7a042
sriramsv <sriramsv1991@gmail.com>
2017-05-30 00:15:59 -0700

    Fix

--- my_assist/reminder/views.py
+++ my_assist/reminder/views.py
@@ -4,6 +4,6 @@ from flask import Blueprint,jsonify,request
 import requests,json
 from .models import statereminder
 from my_assist.extensions import manager
-from models import db
+from reminder.models import db
 
 blueprint=manager.create_api_blueprint('statereminder',statereminder,methods=['GET','POST','PATCH','DELETE'])
