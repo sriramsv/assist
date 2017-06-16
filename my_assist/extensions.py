5650becdf89aa119a3d7d8b76f777cf026c57d99
sriramsv <sriramsv1991@gmail.com>
2017-06-15 20:18:56 -0700

    Fix Redis

--- my_assist/extensions.py
+++ my_assist/extensions.py
@@ -4,11 +4,7 @@ from flask_assistant import Assistant
 from flask_sqlalchemy import SQLAlchemy
 from flask_restless import APIManager
 import os
-from redis import StrictRedis as Redis
 
-
-redisurl=os.getenv("REDISTOGO_URL")
 db = SQLAlchemy()
 assist = Assistant()
-cache=Redis.from_url(redisurl)
 manager=APIManager(flask_sqlalchemy_db=db)
